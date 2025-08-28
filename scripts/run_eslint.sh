49317b2033b2c621af87d6e4d57947e3e1738ac9
Srini Arul David <srinivasan.arulsivam@macys.com>
2025-08-28 11:59:49 -0700

    Feat: Refactor pre-commit setup with .pre-commit-hooks.yaml and scripts

---
+++ scripts/run_eslint.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Assumes eslint is installed globally or in project's node_modules
+exec eslint "$@"
---
+++ scripts/run_prettier.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Assumes prettier is installed globally or in project's node_modules
+exec prettier "$@"
